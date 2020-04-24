0ee58113594e638d0cb6f8f5f9f65798c7e3a201
Anand Jain <anandjainsa@gmail.com>
2020-04-24 09:27:39 +0000

    added echo

--- gitrelease.sh
+++ gitrelease.sh
@@ -4,13 +4,18 @@ developmentVersion=$2
 gitbranch=$3
 
 # go to the master branch
+echo "1"
 git checkout ${gitbranch}
+echo "2"
 git merge --no-ff -m "US00000 $scmCommentPrefix Merge release/$releaseVersion into $gitbranch" release/$releaseVersion
 
+echo "3"
 # Removing the release branch
 git branch -D release/$releaseVersion
+echo "4"
 
 git push origin ${gitbranch} --tags
+echo "6"
 #git push origin ${gitbranch} && git push --tags
 #git push --push-option=USJenkins${releaseVersion} origin ${gitbranch} && git push --tags
 
