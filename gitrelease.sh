fda74071ffd586e58e739ba110a94fab177db4c3
Anand Jain <anandjainsa@gmail.com>
2020-04-24 09:44:27 +0000

    added git tag

--- gitrelease.sh
+++ gitrelease.sh
@@ -4,18 +4,17 @@ developmentVersion=$2
 gitbranch=$3
 
 # go to the master branch
-echo "1"
 git checkout ${gitbranch}
-echo "2"
 git merge --no-ff -m "US00000 $scmCommentPrefix Merge release/$releaseVersion into $gitbranch" release/$releaseVersion
 
-echo "3"
+# Tagging release branch
+git tag -a v$releaseVersion -m "My version v$releaseVersion"
+
 # Removing the release branch
 git branch -D release/$releaseVersion
-echo "4"
+
 
 git push origin ${gitbranch} --tags
-echo "6"
 #git push origin ${gitbranch} && git push --tags
 #git push --push-option=USJenkins${releaseVersion} origin ${gitbranch} && git push --tags
 
