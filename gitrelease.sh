84ff8005db9105a42c07f96e2940844018da0646
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-04-25 12:05:17 +0530

    Update gitrelease.sh

--- gitrelease.sh
+++ gitrelease.sh
@@ -9,8 +9,7 @@ git merge --no-ff -m "US00000 $scmCommentPrefix Merge release/$releaseVersion in
 git tag -a v$releaseVersion -m "Commit version v$releaseVersion"
 
 # Removing the release branch
-git branch -D release/$releaseVersion
-git branch -D develop
+git branch -D release/$releaseVersion develop
 
 git push origin ${gitbranch} --tags
 #git push origin ${gitbranch} && git push --tags
